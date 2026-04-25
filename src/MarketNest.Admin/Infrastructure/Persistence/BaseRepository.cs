eac7771b2c1d3019fe652993a070f635088b1752
Tran Nha Hoang <trannhahoang1909@gmail.com>
2026-04-26 03:43:37 +0800

    feat(admin): add BaseRepository and BaseQuery abstract classes

---
+++ src/MarketNest.Admin/Infrastructure/Persistence/BaseRepository.cs
@@ -0,0 +1,30 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using MarketNest.Core.Common;
+using MarketNest.Core.Common.Persistence;
+
+namespace MarketNest.Admin.Infrastructure;
+
+public abstract class BaseRepository<TEntity, TKey>(AdminDbContext db)
+    : IBaseRepository<TEntity, TKey>
+    where TEntity : Entity<TKey>
+{
+    protected AdminDbContext Db => db;
+
+    public virtual Task<TEntity?> GetByKeyAsync(TKey id, CancellationToken ct = default)
+        => db.Set<TEntity>().FirstOrDefaultAsync(e => e.Id!.Equals(id), ct);
+
+    public virtual async Task<TEntity> GetByKeyOrThrowAsync(TKey id, CancellationToken ct = default)
+        => await GetByKeyAsync(id, ct)
+           ?? throw new KeyNotFoundException($"{typeof(TEntity).Name} '{id}' not found");
+
+    public virtual Task<bool> ExistsAsync(TKey id, CancellationToken ct = default)
+        => db.Set<TEntity>().AnyAsync(e => e.Id!.Equals(id), ct);
+
+    public virtual void Add(TEntity entity)    => db.Set<TEntity>().Add(entity);
+    public virtual void Update(TEntity entity) => db.Set<TEntity>().Update(entity);
+    public virtual void Remove(TEntity entity) => db.Set<TEntity>().Remove(entity);
+
+    public Task<int> SaveChangesAsync(CancellationToken ct = default)
+        => db.SaveChangesAsync(ct);
+}
