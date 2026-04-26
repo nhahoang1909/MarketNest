78f2028f961d2889202fe6dfabd1b74bca0c63ce
Tran Nha Hoang <trannhahoang1909@gmail.com>
2026-04-26 23:10:16 +0800

    refactor(logging): migrate Auth pages to [LoggerMessage]

--- src/MarketNest.Web/Pages/Auth/ForgotPassword.cshtml.cs
+++ src/MarketNest.Web/Pages/Auth/ForgotPassword.cshtml.cs
@@ -2,15 +2,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MarketNest.Web.Pages.Auth;
 
-public class ForgotPasswordModel(IAppLogger<ForgotPasswordModel> logger) : PageModel
+public partial class ForgotPasswordModel(IAppLogger<ForgotPasswordModel> logger) : PageModel
 {
     public void OnGet()
-    {
-        logger.Info("API {Api} Start - CorrelationId={Cid}", nameof(OnGet), HttpContext?.TraceIdentifier ?? "-");
-    }
+        => Log.InfoOnGet(logger, HttpContext?.TraceIdentifier ?? "-");
 
     public void OnPost()
+        => Log.InfoOnPost(logger, HttpContext?.TraceIdentifier ?? "-");
+
+    private static partial class Log
     {
-        logger.Info("API {Api} Start - CorrelationId={Cid}", nameof(OnPost), HttpContext?.TraceIdentifier ?? "-");
+        [LoggerMessage((int)LogEventId.AuthForgotPasswordStart, LogLevel.Information,
+            "ForgotPassword OnGet Start - CorrelationId={CorrelationId}")]
+        public static partial void InfoOnGet(ILogger logger, string correlationId);
+
+        [LoggerMessage((int)LogEventId.AuthForgotPasswordStart + 1, LogLevel.Information,
+            "ForgotPassword OnPost Start - CorrelationId={CorrelationId}")]
+        public static partial void InfoOnPost(ILogger logger, string correlationId);
     }
 }
