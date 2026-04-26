1095df3bca7d5666e80d20a5baaa1c77892406ae
Tran Nha Hoang <trannhahoang1909@gmail.com>
2026-04-26 23:23:06 +0800

    refactor(logging): convert Seller/Orders + Orders/Confirmation to [LoggerMessage]

--- src/MarketNest.Web/Pages/Orders/Confirmation.cshtml.cs
+++ src/MarketNest.Web/Pages/Orders/Confirmation.cshtml.cs
@@ -2,14 +2,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MarketNest.Web.Pages.Orders;
 
-public class ConfirmationModel(IAppLogger<ConfirmationModel> logger) : PageModel
+public partial class ConfirmationModel(IAppLogger<ConfirmationModel> logger) : PageModel
 {
     [BindProperty(SupportsGet = true)] public Guid OrderId { get; set; }
 
     public void OnGet()
+        => Log.InfoOnGet(logger, OrderId, HttpContext?.TraceIdentifier ?? "-");
+
+    private static partial class Log
     {
-        var cid = HttpContext?.TraceIdentifier ?? "-";
-        using var scope = logger.BeginApiScope(nameof(OnGet), new { OrderId }, cid);
-        scope.Success();
+        [LoggerMessage((int)LogEventId.OrdersConfirmationStart, LogLevel.Information,
+            "OrderConfirmation OnGet Start - OrderId={OrderId} CorrelationId={CorrelationId}")]
+        public static partial void InfoOnGet(ILogger logger, Guid orderId, string correlationId);
     }
 }
