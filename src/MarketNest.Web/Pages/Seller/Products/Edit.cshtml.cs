9432829a624f98838ff91807375b148ee48a7663
Tran Nha Hoang <trannhahoang1909@gmail.com>
2026-04-26 23:21:49 +0800

    refactor(logging): convert Seller/Products pages from BeginApiScope to [LoggerMessage]

--- src/MarketNest.Web/Pages/Seller/Products/Edit.cshtml.cs
+++ src/MarketNest.Web/Pages/Seller/Products/Edit.cshtml.cs
@@ -2,21 +2,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MarketNest.Web.Pages.Seller.Products;
 
-public class EditModel(IAppLogger<EditModel> logger) : PageModel
+public partial class EditModel(IAppLogger<EditModel> logger) : PageModel
 {
     [BindProperty(SupportsGet = true)] public Guid ProductId { get; set; }
 
     public void OnGet()
-    {
-        var cid = HttpContext?.TraceIdentifier ?? "-";
-        using var scope = logger.BeginApiScope(nameof(OnGet), new { ProductId }, cid);
-        scope.Success();
-    }
+        => Log.InfoOnGet(logger, ProductId, HttpContext?.TraceIdentifier ?? "-");
 
     public void OnPost()
+        => Log.InfoOnPost(logger, ProductId, HttpContext?.TraceIdentifier ?? "-");
+
+    private static partial class Log
     {
-        var cid = HttpContext?.TraceIdentifier ?? "-";
-        using var scope = logger.BeginApiScope(nameof(OnPost), new { ProductId }, cid);
-        scope.Success();
+        [LoggerMessage((int)LogEventId.SellerProductsEditStart, LogLevel.Information,
+            "SellerProductEdit OnGet Start - ProductId={ProductId} CorrelationId={CorrelationId}")]
+        public static partial void InfoOnGet(ILogger logger, Guid productId, string correlationId);
+
+        [LoggerMessage((int)LogEventId.SellerProductsEditStart + 1, LogLevel.Information,
+            "SellerProductEdit OnPost Start - ProductId={ProductId} CorrelationId={CorrelationId}")]
+        public static partial void InfoOnPost(ILogger logger, Guid productId, string correlationId);
     }
 }
