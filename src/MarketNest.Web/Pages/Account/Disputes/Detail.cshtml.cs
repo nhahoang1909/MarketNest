7a81ebf17b64a144e3739370682c1baf9acf6fb1
Tran Nha Hoang <trannhahoang1909@gmail.com>
2026-04-26 23:10:18 +0800

    refactor(logging): migrate Account pages to [LoggerMessage]

--- src/MarketNest.Web/Pages/Account/Disputes/Detail.cshtml.cs
+++ src/MarketNest.Web/Pages/Account/Disputes/Detail.cshtml.cs
@@ -2,12 +2,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MarketNest.Web.Pages.Account.Disputes;
 
-public class DetailModel(IAppLogger<DetailModel> logger) : PageModel
+public partial class DetailModel(IAppLogger<DetailModel> logger) : PageModel
 {
     [BindProperty(SupportsGet = true)] public Guid DisputeId { get; set; }
 
     public void OnGet()
+        => Log.InfoOnGet(logger, DisputeId, HttpContext?.TraceIdentifier ?? "-");
+
+    private static partial class Log
     {
-        logger.Info("API {Api} Start - CorrelationId={Cid} Payload={Payload}", nameof(OnGet), HttpContext?.TraceIdentifier ?? "-", new { DisputeId });
+        [LoggerMessage((int)LogEventId.AccountDisputesDetailStart, LogLevel.Information,
+            "AccountDisputeDetail OnGet Start - DisputeId={DisputeId} CorrelationId={CorrelationId}")]
+        public static partial void InfoOnGet(ILogger logger, Guid disputeId, string correlationId);
     }
 }
--- src/MarketNest.Web/Pages/Account/Orders/Detail.cshtml.cs
+++ src/MarketNest.Web/Pages/Account/Orders/Detail.cshtml.cs
@@ -2,12 +2,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MarketNest.Web.Pages.Account.Orders;
 
-public class DetailModel(IAppLogger<DetailModel> logger) : PageModel
+public partial class DetailModel(IAppLogger<DetailModel> logger) : PageModel
 {
     [BindProperty(SupportsGet = true)] public Guid OrderId { get; set; }
 
     public void OnGet()
+        => Log.InfoOnGet(logger, OrderId, HttpContext?.TraceIdentifier ?? "-");
+
+    private static partial class Log
     {
-        logger.Info("API {Api} Start - CorrelationId={Cid} Payload={Payload}", nameof(OnGet), HttpContext?.TraceIdentifier ?? "-", new { OrderId });
+        [LoggerMessage((int)LogEventId.AccountOrdersDetailStart, LogLevel.Information,
+            "AccountOrderDetail OnGet Start - OrderId={OrderId} CorrelationId={CorrelationId}")]
+        public static partial void InfoOnGet(ILogger logger, Guid orderId, string correlationId);
     }
 }
